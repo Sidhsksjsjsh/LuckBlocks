79a78fa16f5a3377b83d6e5a4d10e2436d2e0f2a
Terminated <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-04-10 00:52:16 +0900

    Update Script.lua

--- TurtleHub/Script.lua
+++ TurtleHub/Script.lua
@@ -9,6 +9,14 @@ for _,EggSync in pairs(game:GetService("Workspace").Core.Eggs:GetChildren()) do
     table.insert(AsyncEgg, EggSync.Name)
 end
 
+local SyncDelete = {
+       Pet_1 = "",
+       Pet_2 = "",
+       Pet_3 = "",
+       Pet_4 = "",
+       Pet_5 = ""
+}
+
 -- Variables holding time to wait (These are the default time values)
 local moneyWait = 0.1
 local rebirthWait = 0.1
@@ -72,7 +80,13 @@ function buyEgg(time)
            local args = {
                [1] = eggName,
                [2] = eggAmt,
-               [3] = {}
+               [3] = {
+                  [1] = SyncDelete.Pet_1,
+                  [2] = SyncDelete.Pet_2,
+                  [3] = SyncDelete.Pet_3,
+                  [4] = SyncDelete.Pet_4,
+                  [5] = SyncDelete.Pet_5
+               }
            }
 
            game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Remotes"):WaitForChild(
@@ -98,6 +112,90 @@ local E = Window:MakeTab({
 Name = "Egg",
 Icon = "rbxassetid://12972185540",
 PremiumOnly = false
+})
+
+local TP = Window:MakeTab({
+Name = "Teleport",
+Icon = "rbxassetid://12972185540",
+PremiumOnly = false
+})
+
+local Cfg = Window:MakeTab({
+Name = "Config",
+Icon = "rbxassetid://13040495457",
+PremiumOnly = false
+})
+
+local S1 = Cfg:AddSection({
+Name = "Max Pet Equip"
+})
+
+local S2 = Cfg:AddSection({
+Name = "Max Sword Equip"
+})
+
+local S3 = Cfg:AddSection({
+Name = "Rebirth"
+})
+
+local S4 = Cfg:AddSection({
+Name = "DPS | Damage Per Second"
+})
+
+local SyncConfigFunction = {
+      Inf = 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999,
+      Custom = 0
+}
+
+S1:AddButton({
+Name = "Inf Pet Max Equip",
+Callback = function()
+      game.Players.LocalPlayer.Stats.MaxPetEquipped.Value = SyncConfigFunction.Inf
+  end    
+})
+
+S2:AddButton({
+Name = "Inf Sword Max Equip",
+Callback = function()
+      game.Players.LocalPlayer.Stats.MaxEquipSpace.Value = SyncConfigFunction.Inf
+  end    
+})
+
+S3:AddButton({
+Name = "Inf Rebirth",
+Callback = function()
+      game.Players.LocalPlayer.Stats.Rebirths.Value = SyncConfigFunction.Inf
+  end    
+})
+
+S4:AddButton({
+Name = "Inf DPS",
+Callback = function()
+      game.Players.LocalPlayer.Stats["⚔️DPS"].Value = SyncConfigFunction.Inf
+  end    
+})
+
+function SyncNotify(SyncName, SyncContent)
+OrionLib:MakeNotification({
+Name = SyncName,
+Content = SyncContent,
+Image = "rbxassetid://4483345998",
+Time = 5
+})
+end
+
+TP:AddButton({
+Name = "Anime World (REBIRTH REQUIRED: 2)",
+Callback = function()
+if game.Players.LocalPlayer.Stats.Rebirths < 2 then
+SyncNotify("rebirth is not enough", "Your rebirth is below 2, hurry up for the rebirth!")
+else
+local args = {
+      [1] = "Anime"
+    }
+game:GetService("ReplicatedStorage").Events.Remotes.ServerRemotes.TeleportZone:InvokeServer(unpack(args))
+end
+  end    
 })
 
 -- You have no enemies tho
@@ -202,6 +300,53 @@ end
 })
 
 -- Egg dropdown
+
+-- SyncDelete.Pet_1 2, 3, 4, 5
+
+E:AddTextbox({
+Name = "Auto Delete (1)",
+Default = "?",
+TextDisappear = false,
+Callback = function(value)
+   SyncDelete.Pet_1 = value
+end
+})
+
+E:AddTextbox({
+Name = "Auto Delete (2)",
+Default = "?",
+TextDisappear = false,
+Callback = function(value)
+   SyncDelete.Pet_2 = value
+end
+})
+
+E:AddTextbox({
+Name = "Auto Delete (3)",
+Default = "?",
+TextDisappear = false,
+Callback = function(value)
+   SyncDelete.Pet_3 = value
+end
+})
+
+E:AddTextbox({
+Name = "Auto Delete (4)",
+Default = "?",
+TextDisappear = false,
+Callback = function(value)
+   SyncDelete.Pet_4 = value
+end
+})
+
+E:AddTextbox({
+Name = "Auto Delete (5)",
+Default = "?",
+TextDisappear = false,
+Callback = function(value)
+   SyncDelete.Pet_5 = value
+end
+})
 
 E:AddDropdown({
 Name = "Select Egg",
